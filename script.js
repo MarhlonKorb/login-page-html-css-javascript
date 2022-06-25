aea82141e247851d00ba312be0264eb9c9409a7c
Marhlon Korb <marhlonkorb@hotmail.com>
2022-06-25 17:33:35 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container"),
+pwShowHide = document.querySelectorAll(".showHidePw"),
+pwFields = document.querySelectorAll(".password");
+
+//js code to show/hide password and change icon
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        pwFields.forEach(pwFields => {
+            if(pwFields.type ="password"){
+                pwFields.type = "text";
+            } else {
+                pwFields.type = "password";
+            }
+        })
+    })
+})
